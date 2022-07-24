d7ea29224847ea5e9a06b087460624e6b13b22df
DELL <DELL@DESKTOP-6UO4B1H>
2022-07-23 20:38:47 -0700

    Add project files.

---
+++ CRUDusingDrapperFremwork/Controllers/ProductController.cs
@@ -0,0 +1,106 @@
+﻿using CRUDusingDrapperFremwork.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDusingDrapperFremwork.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public ActionResult List()
+        {
+            var model = ProductDAL.GetAllProducts();
+            return View(model);
+        }
+
+        // GET: ProductController/Details/5
+        public ActionResult Details(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+        }
+
+        // GET: ProductController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product prod)
+        {
+            try
+            {
+                int result = ProductDAL.AddProduct(prod);
+                if (result == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+        }
+
+        // POST: ProductController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product prod)
+        {
+            try
+            {
+                int result = ProductDAL.UpdateProduct(prod);
+                if (result == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Product prod = ProductDAL.GetProductById(id);
+            return View(prod);
+        }
+
+        // POST: ProductController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                int result = ProductDAL.DeleteProduct(id);
+                if (result == 1)
+                    return RedirectToAction(nameof(List));
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUDusingDrapperFremwork/Models/Product.cs
@@ -0,0 +1,103 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDusingDrapperFremwork.Models
+{
+    public class Product
+    {
+            [Key]
+            [ScaffoldColumn(false)]
+            public int Id { get; set; }
+            [Required(ErrorMessage = "Name is required")]
+            [Display(Name = "Product Name")]
+            public string Name { get; set; }
+            [Required(ErrorMessage = "Price is required")]
+            [Display(Name = "Product Price")]
+            public double Price { get; set; }
+
+            public static string ConnectionString = "Server=DESKTOP-6UO4B1H\\SQLEXPRESS;Database=SP;Integrated Security=True;";
+        }
+        public class ProductDAL
+        {
+            public static IEnumerable<Product> GetAllProducts()
+            {
+                List<Product> productList = new List<Product>();
+                using (IDbConnection con = new SqlConnection(Product.ConnectionString))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    productList = con.Query<Product>("SP_Select_Product").ToList();
+
+                }
+                return productList;
+            }
+            public static Product GetProductById(int id)
+            {
+                Product product = new Product();
+                using (IDbConnection con = new SqlConnection(Product.ConnectionString))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@id", id);
+                    product = con.Query<Product>("SP_SearchById_Product", dynamicParameters, commandType: CommandType.StoredProcedure).FirstOrDefault();
+
+                }
+                return product;
+            }
+
+            public static int AddProduct(Product prod)
+            {
+                int result = 0;
+                using (IDbConnection con = new SqlConnection(Product.ConnectionString))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@name", prod.Name);
+                    dynamicParameters.Add("@price", prod.Price);
+                    result = con.Execute("SP_Insert_Product", dynamicParameters, commandType: CommandType.StoredProcedure);
+                }
+                return result;
+            }
+
+            public static int UpdateProduct(Product prod)
+            {
+                int result = 0;
+                using (IDbConnection con = new SqlConnection(Product.ConnectionString))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@id", prod.Id);
+                    dynamicParameters.Add("@name", prod.Name);
+                    dynamicParameters.Add("@price", prod.Price);
+                    result = con.Execute("SP_Update_Product", dynamicParameters, commandType: CommandType.StoredProcedure);
+                }
+                return result;
+            }
+
+            public static int DeleteProduct(int id)
+            {
+                int result = 0;
+                using (IDbConnection con = new SqlConnection(Product.ConnectionString))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+                    DynamicParameters dynamicParameters = new DynamicParameters();
+                    dynamicParameters.Add("@id", id);
+                    result = con.Execute("SP_Delete_Product", dynamicParameters, commandType: CommandType.StoredProcedure);
+                }
+                return result;
+            }
+        }
+
+
+    
+}
